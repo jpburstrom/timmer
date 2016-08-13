5dcef62bfedbd4fb18d382cb8cbb52b68268e586
Johannes Burström <johannes@ljud.org>
2016-08-13 12:21:01 +0200

    Update fetch-sounds script

--- player/fetch-sounds.sh
+++ player/fetch-sounds.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git annex init &&
-git annex upgrade &&
-git annex enableremote timmers3 &&
-git annex sync --content
+git-annex init &&
+git-annex upgrade &&
+git-annex enableremote timmers3 &&
+git-annex get
